a748e9c7ca1d559be6caff1747ad341cbd389100
Luca Dalla Valle <luca.dallavalle@outlook.com>
2020-11-29 19:54:55 +0100

    implement solution for the "Robot Name" exercise

---
+++ robot-name/robot_name.py
@@ -0,0 +1,26 @@
+import random
+
+
+class Robot:
+    name = ""
+
+    def __init__(self):
+        """Initializes the 'name' field with a random value."""
+        self.name = self.generate_random_name()
+
+    def generate_random_name(self) -> str:
+        """Generates a random name following a specific format.
+
+        The format used to create the random name is composed by
+        two letters followed by 3 digits. Examples: AB123, LS104.
+
+        Returns:
+            The generated name.
+        """
+        valid_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        r = random.Random()
+        return f"{valid_letters[r.randint(0, 25)]}{valid_letters[r.randint(0, 25)]}{r.randint(0, 999):03d}"
+
+    def reset(self):
+        """Sets the 'name' field to a new random value."""
+        self.name = self.generate_random_name()
