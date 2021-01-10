a81a9ebfd507d9b51c0b782cf7cf61cfcbabbd85
Luca Dalla Valle <luca.dallavalle@outlook.com>
2021-01-10 20:09:20 +0100

    refactor 'matrix' exercise

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -2,17 +2,14 @@ from typing import Dict, List
 
 
 class Matrix:
-    _rows: Dict[int, List[int]]
+    _rows = []
 
     def __init__(self, matrix_string: str):
-        self._rows = {}
-        index = 0
-        for input_row in [i for i in matrix_string.split("\n")]:
-            self._rows.update({index: [int(i) for i in input_row.split(" ")]})
-            index += 1
+        for r in [i for i in matrix_string.splitlines()]:
+            self._rows.append([int(x) for x in r.split(" ")])
 
     def row(self, index: int) -> List[int]:
-        return self._rows[index-1]
+        return self._rows[index - 1].copy()
 
     def column(self, index: int) -> List[int]:
-        return [i[index-1] for i in self._rows.values()]
+        return [i[index - 1] for i in self._rows]
